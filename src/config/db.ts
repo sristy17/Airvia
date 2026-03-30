a22940f941a925f552eb3fc6fa8afaaeca37fd8e
sristy17 <sristypaul1705@gmail.com>
2026-03-30 20:22:38 +0530

    minor fixes

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,9 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-  user: "admin",          
-  host: "localhost",
-  database: "airbnb",     
-  password: "admin",      
-  port: 5432,
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
-
-export default pool;
--- src/repositories/flight.repository.ts
+++ src/repositories/flight.repository.ts
@@ -1,4 +1,4 @@
-import pool from "../config/db.js";
+import { pool } from "../config/db.js";
 import { CreateFlightDTO, Flight } from "../services/flights/dto/flight.dto.js";
 
 export const createFlight = async (data: CreateFlightDTO): Promise<Flight> => {
